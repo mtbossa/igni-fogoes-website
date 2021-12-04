cfa6a8989fba4f712894e645f5ebfcfe1fd2e408
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-04 10:33:52 -0300

    Change folder from styles to css. Menu working.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+  const $navbarList = document.querySelector(".navbar__list");
+  const $buttonOpenMenu = document.querySelector(".navbar__button-open");
+  const $buttonCloseMenu = document.querySelector(".navbar__button-close");
+  const $menuLinks = document.querySelectorAll(".navbar__link");
+
+  $buttonOpenMenu.addEventListener("click", () => {
+    $navbarList.classList.add("navbar__list--visible");
+    $buttonCloseMenu.classList.add("navbar__button-close--visible");
+  });
+
+  $buttonCloseMenu.addEventListener("click", () => {
+    $navbarList.classList.remove("navbar__list--visible");
+    $buttonCloseMenu.classList.remove("navbar__button-close--visible");
+  });
+
+  $menuLinks.forEach(($e) => {
+    $e.addEventListener("click", () => {
+      $navbarList.classList.remove("navbar__list--visible");
+      $buttonCloseMenu.classList.remove("navbar__button-close--visible");
+    });
+  });
+};
