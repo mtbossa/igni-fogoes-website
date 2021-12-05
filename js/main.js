69e1c29ef7b87c311edaddec9d0af3ceb24d5e3d
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-05 20:24:19 -0300

    Add nav shadow when scrolling.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 window.onload = () => {
+  showCloseMenu();
+  scrollShadow();
+};
+
+/* Open and close the menu */
+const showCloseMenu = () => {
   const $navbarList = document.querySelector(".navbar__list");
   const $buttonOpenMenu = document.querySelector(".navbar__button-open");
   const $buttonCloseMenu = document.querySelector(".navbar__button-close");
   const $menuLinks = document.querySelectorAll(".navbar__link");
 
   $buttonOpenMenu.addEventListener("click", () => {
-    $navbarList.classList.add("navbar__list--visible");
-    $buttonCloseMenu.classList.add("navbar__button-close--visible");
+    $navbarList.classList.toggle("navbar__list--visible");
+    $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
   });
 
   $buttonCloseMenu.addEventListener("click", () => {
-    $navbarList.classList.remove("navbar__list--visible");
-    $buttonCloseMenu.classList.remove("navbar__button-close--visible");
+    $navbarList.classList.toggle("navbar__list--visible");
+    $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
   });
 
   $menuLinks.forEach(($element) => {
     $element.addEventListener("click", () => {
-      $navbarList.classList.remove("navbar__list--visible");
-      $buttonCloseMenu.classList.remove("navbar__button-close--visible");
+      $navbarList.classList.toggle("navbar__list--visible");
+      $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
     });
+  });
+};
+
+/* Scroll shadow */
+const scrollShadow = () => {
+  const header = document.querySelector(".header");
+  const navHeight = header.offsetHeight;
+
+  window.addEventListener("scroll", () => {
+    window.scrollY >= navHeight
+      ? header.classList.add("header--shadow")
+      : header.classList.remove("header--shadow");
   });
 };
