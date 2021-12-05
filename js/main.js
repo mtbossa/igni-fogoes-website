552e141267ee56fe6045e9f13cb4b20db88b0927
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-05 20:36:32 -0300

    Change prettierrc file. Add Scroll Reveal.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,58 @@
 window.onload = () => {
-  showCloseMenu();
-  scrollShadow();
-};
+	showCloseMenu()
+	scrollShadow()
+
+	/* ScrollReveal: show elements when scrolling */
+	const scrollReveal = ScrollReveal({
+		origin: 'top',
+		distance: '30px',
+		duration: 700,
+		reset: true
+	})
+
+	scrollReveal.reveal(
+		`
+  .section__image, .button, .service-card
+`,
+		{
+			interval: 100
+		}
+	)
+}
 
 /* Open and close the menu */
 const showCloseMenu = () => {
-  const $navbarList = document.querySelector(".navbar__list");
-  const $buttonOpenMenu = document.querySelector(".navbar__button-open");
-  const $buttonCloseMenu = document.querySelector(".navbar__button-close");
-  const $menuLinks = document.querySelectorAll(".navbar__link");
+	const $navbarList = document.querySelector('.navbar__list')
+	const $buttonOpenMenu = document.querySelector('.navbar__button-open')
+	const $buttonCloseMenu = document.querySelector('.navbar__button-close')
+	const $menuLinks = document.querySelectorAll('.navbar__link')
 
-  $buttonOpenMenu.addEventListener("click", () => {
-    $navbarList.classList.toggle("navbar__list--visible");
-    $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
-  });
+	$buttonOpenMenu.addEventListener('click', () => {
+		$navbarList.classList.toggle('navbar__list--visible')
+		$buttonCloseMenu.classList.toggle('navbar__button-close--visible')
+	})
 
-  $buttonCloseMenu.addEventListener("click", () => {
-    $navbarList.classList.toggle("navbar__list--visible");
-    $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
-  });
+	$buttonCloseMenu.addEventListener('click', () => {
+		$navbarList.classList.toggle('navbar__list--visible')
+		$buttonCloseMenu.classList.toggle('navbar__button-close--visible')
+	})
 
-  $menuLinks.forEach(($element) => {
-    $element.addEventListener("click", () => {
-      $navbarList.classList.toggle("navbar__list--visible");
-      $buttonCloseMenu.classList.toggle("navbar__button-close--visible");
-    });
-  });
-};
+	$menuLinks.forEach($element => {
+		$element.addEventListener('click', () => {
+			$navbarList.classList.toggle('navbar__list--visible')
+			$buttonCloseMenu.classList.toggle('navbar__button-close--visible')
+		})
+	})
+}
 
 /* Scroll shadow */
 const scrollShadow = () => {
-  const header = document.querySelector(".header");
-  const navHeight = header.offsetHeight;
+	const header = document.querySelector('.header')
+	const navHeight = header.offsetHeight
 
-  window.addEventListener("scroll", () => {
-    window.scrollY >= navHeight
-      ? header.classList.add("header--shadow")
-      : header.classList.remove("header--shadow");
-  });
-};
+	window.addEventListener('scroll', () => {
+		window.scrollY >= navHeight
+			? header.classList.add('header--shadow')
+			: header.classList.remove('header--shadow')
+	})
+}
