680a84ba299fcc89209d5b6c8d027ea9aa334112
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-07 11:09:42 -0300

    Add animation when user enters in a section. Change about text.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
-window.onload = () => {
-	showCloseMenu()
-	scrollShadow()
+// Variables
+/* ScrollReveal: show elements when scrolling */
+const scrollReveal = ScrollReveal({
+	origin: 'top',
+	distance: '30px',
+	duration: 700,
+	reset: true
+})
+const $header = document.querySelector('.header')
+const $sections = document.querySelectorAll('section[id]')
+const navHeight = $header.offsetHeight
 
-	/* ScrollReveal: show elements when scrolling */
-	const scrollReveal = ScrollReveal({
-		origin: 'top',
-		distance: '30px',
-		duration: 700,
-		reset: true
-	})
-
-	scrollReveal.reveal(
-		`
-  .section__image, .button, .service-card
-`,
-		{
-			interval: 100
-		}
-	)
-}
-
+// Functions
 /* Open and close the menu */
 const showCloseMenu = () => {
 	const $navbarList = document.querySelector('.navbar__list')
@@ -47,12 +38,47 @@ const showCloseMenu = () => {
 
 /* Scroll shadow */
 const scrollShadow = () => {
-	const header = document.querySelector('.header')
-	const navHeight = header.offsetHeight
+	window.scrollY >= navHeight
+		? $header.classList.add('header--shadow')
+		: $header.classList.remove('header--shadow')
+}
 
-	window.addEventListener('scroll', () => {
-		window.scrollY >= navHeight
-			? header.classList.add('header--shadow')
-			: header.classList.remove('header--shadow')
+const activateMenuAtCurrentSection = () => {
+	const checkpoint = window.scrollY + (window.innerHeight / 8) * 4
+
+	$sections.forEach($element => {
+		const sectionTop = $element.offsetTop
+		const sectionHeight = $element.offsetHeight
+		const sectionId = $element.getAttribute('id')
+
+		const checkpointStart = checkpoint >= sectionTop
+		const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+		if(checkpointStart && checkpointEnd) {
+			document
+				.querySelector(`.navbar__link[href*=${sectionId}]`)
+				.classList.add('navbar__link--active')
+		} else {
+			document
+			.querySelector(`.navbar__link[href*=${sectionId}]`)
+			.classList.remove('navbar__link--active')
+		}
 	})
 }
+
+// Execution
+window.addEventListener('scroll', () => {
+	scrollShadow()
+	activateMenuAtCurrentSection()
+})
+
+showCloseMenu()
+
+scrollReveal.reveal(
+	`
+  		.section__image, .button, .service-card
+		`,
+	{
+		interval: 100
+	}
+)
